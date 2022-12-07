41d72d9a924f11f442cb31b3bd14cbad6cc472c2
bluejhonny777@gmail.com <bluejhonny777@gmail.com>
2022-12-07 16:55:21 -0400

    cron time gmt

--- scraper/index.js
+++ scraper/index.js
@@ -3,13 +3,14 @@ const { busquedaComputrabajo } = require('./scraperComputrabajo');
 const { busquedaBumeran } = require('./scraperBumeran');
 
 const cronJobScrapper = () => {
+  // cron Time is GMT
   var hours = (new Date()).getHours();
   var minutes = (new Date()).getMinutes();
   console.log(hours + " : " + minutes);
   console.log('Scraper Start with Cron')
   new cron.CronJob(
     //'5 14 1-31 0-11 0-6',
-    '56 16 1-31 0-11 0-6',
+    '01 21 1-31 0-11 0-6',
     async () => {
       console.log('Ejecutando...');
       console.log(new Date().toLocaleString());
