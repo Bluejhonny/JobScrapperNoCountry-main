d78f207d9c49fcfa3cb074acdef020f612650822
bluejhonny777@gmail.com <bluejhonny777@gmail.com>
2022-12-08 18:05:55 -0400

    index cron edited

--- scraper/index.js
+++ scraper/index.js
@@ -10,7 +10,7 @@ const cronJobScrapper = () => {
   console.log('Scraper Start with Cron')
   new cron.CronJob(
     //'55 14 1-31 0-11 0-6',
-    '00 22 1-31 0-11 0-6',
+    '10 22 1-31 0-11 0-6',
     async () => {
       console.log('Ejecutando...');
       console.log(new Date().toLocaleString());
