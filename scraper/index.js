3280cd6f55d861506502024d42f70c1edabcb0b6
bluejhonny777@gmail.com <bluejhonny777@gmail.com>
2023-02-09 01:01:50 -0400

    Cron setting time

--- scraper/index.js
+++ scraper/index.js
@@ -10,8 +10,7 @@ const cronJobScrapper = () => {
   console.log('Scraper Start with Cron')
   new cron.CronJob(
     // min hour date month day
-    //'00 04 1-31 0-11 0-6',
-    '* * * * * *',
+    '04 05 1-31 0-11 0-6',
     async () => {
       console.log('Ejecutando...');
       console.log(new Date().toLocaleString());
