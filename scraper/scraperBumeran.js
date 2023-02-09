8c928210a5c0b748fc07535757e5331fa9439001
bluejhonny777@gmail.com <bluejhonny777@gmail.com>
2023-02-09 16:49:37 -0400

    bumeran browser disable

--- scraper/scraperBumeran.js
+++ scraper/scraperBumeran.js
@@ -124,7 +124,7 @@ const busquedaBumeran = async (search, location = '') => {
       } else {
         items.push({ name, company, location, type, links, description, source });
         listLinks.push(links);
-        //browserDesc.close()
+        browserDesc.close()
         console.log("next")
       }
       //await browserDesc.close()
@@ -134,7 +134,7 @@ const busquedaBumeran = async (search, location = '') => {
     saveJobs(listLinks, items);
     console.log('Jobs saved');
     stop++;
-    browser.close();
+    //browser.close();
   }
   
   console.log('Busqueda Finalizada');
